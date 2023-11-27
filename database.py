5bd8320a34c76892b610231b0243b70eb57a1f26
Napatr Saengthongsakullert <napatr.se@ku.th>
2023-11-27 23:50:21 +0700

    More code details but not complete ver.

--- database.py
+++ database.py
@@ -1,20 +1,83 @@
 # try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
 
-import csv, os
+import csv, os, copy
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
+with open(os.path.join(__location__, 'persons.csv')) as a:
+    rows = csv.DictReader(a)
     for r in rows:
         persons.append(dict(r))
-print(persons)
+
+login = []
+with open(os.path.join(__location__, 'login.csv')) as b:
+    rows = csv.DictReader(b)
+    for r in rows:
+        login.append(dict(r))
 
 # add in code for a Database class
+class Database:
+    def __init__(self):
+        self.database = []
 
-# add in code for a Table class
+    def insert(self, table):
+        self.database.append(table)
+
+    def search(self, table_name):
+        for table in self.database:
+            if table.table_name == table_name:
+                return table
+
+#I think my DB was done.
+# # add in code for a Table class
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+
+    def join(self, other_table, common_key):
+        joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+        return joined_table
+
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+
+    def aggregate(self, function, aggregation_key):
+        temps = []
+        for item1 in self.table:
+            if self.__is_float(item1[aggregation_key]):
+                temps.append(float(item1[aggregation_key]))
+            else:
+                temps.append(item1[aggregation_key])
+        return function(temps)
+
+    def select(self, attributes_list):
+        temps = []
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
+
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
+
+#I copied it from ProfessorParut in-class exercise.
 
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
 
--- project_manage.py
+++ project_manage.py
@@ -1,34 +1,70 @@
-# import database module
+import database,csv
 
 # define a funcion called initializing
-
 def initializing():
-    pass
-
 # here are things to do in this function:
-
     # create an object to read all csv files that will serve as a persistent state for this program
 
+    # for i in database:
+    #     myFile = open(i.csv, 'w')
+    #     reader = csv.reader(myFile)
+    #     row = [x for x in list_dict[0].keys()]
+    #     reader.writerow(row)
+    #     for dictionary in list_dict:
+    #         reader.writerow(dictionary.values())
+    #     myFile.close()
+    # Not completed yet.
+
     # create all the corresponding tables for those csv files
-
+    Person_table = {"ID":"","First":"","Last":"","Type":""}
+    Login_table = {"ID":"","Username":"","Password":"","Role":""}
+    Project_table = {"ProjectID":"","Title":"","Lead":"","Member1":"","Member2":"","Advisor":"","Status":""}
+    Advisor_table = {"ProjectID":"","to_be_advisor":"","Response":"","Respongse_date":""}
+    Member_table = {"ProjectID":"","to_be_member":"","Response":"","Respongse_date":""}
     # see the guide how many tables are needed
-
     # add all these tables to the database
-
+    DB = database.Database()
+    person = database.Table("Person",Person_table)
+    login = database.Table("Login",Login_table)
+    project = database.Table("Project",Project_table)
+    advisor = database.Table("Advisor",Advisor_table)
+    member = database.Table("Member",Member_table)
+    DB.insert(person)
+    DB.insert(login)
+    DB.insert(project)
+    DB.insert(advisor)
+    DB.insert(member)
 
 # define a funcion called login
-
 def login():
-    pass
-
+    username = input("Please input\nUsername: ")
+    password = input("Password: ")
+    for i in range(len(database.login)):
+        if database.login[i]["username"] == username:
+            if database.login[i]["password"] == password:
+                return [database.login[i]["ID"],database.login[i]["role"]]
+        else:
+            return None
 # here are things to do in this function:
    # add code that performs a login task
         # ask a user for a username and password
         # returns [ID, role] if valid, otherwise returning None
 
+def write_csv(name, list_dict):
+    myFile = open(name.csv, 'w')
+    writer = csv.writer(myFile)
+    row = [x for x in list_dict[0].keys()]
+    writer.writerow(row)
+    for dictionary in list_dict:
+        writer.writerow(dictionary.values())
+    myFile.close()
+
+
+
 # define a function called exit
 def exit():
-    pass
+
+    break
 
 # here are things to do in this function:
    # write out all the tables that have been modified to the corresponding csv files
@@ -39,23 +75,24 @@ def exit():
 
 # make calls to the initializing and login functions defined above
 
-initializing()
+# initializing()
 val = login()
+print(val)
 
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 
 # if val[1] = 'admin':
-    # see and do admin related activities
+#     see and do admin related activities
 # elif val[1] = 'student':
-    # see and do student related activities
+#     see and do student related activities
 # elif val[1] = 'member':
-    # see and do member related activities
+#     see and do member related activities
 # elif val[1] = 'lead':
-    # see and do lead related activities
+#     see and do lead related activities
 # elif val[1] = 'faculty':
-    # see and do faculty related activities
+#     see and do faculty related activities
 # elif val[1] = 'advisor':
-    # see and do advisor related activities
+#     see and do advisor related activities
 
 # once everyhthing is done, make a call to the exit function
 exit()
